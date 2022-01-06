9a2db21ef00a18856df24a78679d04277b9e4e63
kimiyahj <kimiya.h16@gmail.com>
2022-01-06 01:08:53 -0800

    remove console log and set city

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -39,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "d35c7cd4567440639bdda9b1ac775dbe";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Tehran";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
